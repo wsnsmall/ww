b60e0bb67536379b33e875e9ccf56cf47e6eb110
seven small <136567317@qq.com>
2017-07-04 17:43:09 +0800

    修改内容

---
+++ code/js/bullet.js
@@ -0,0 +1,78 @@
+
+
+
+//子弹Bullet
+class Bullet{
+	//属性ele
+	constructor(ele,id){
+	[this.ele,this.id]=[document.createElement("div"),parseInt(Math.random()*100000000)]
+//	this.ele = document.createElement("div");
+//	this.id = parseInt(Math.random()*100000000);
+	}
+	//方法init
+	init(){
+		
+		//添加
+		gameEngine.allBullet[this.id] = this;
+		//console.log( gameEngine.allBullet );
+		
+		gameEngine.ele.appendChild(this.ele);
+		this.ele.className = "bullet";
+		this.ele.style.left = myPlane.ele.offsetLeft + myPlane.ele.offsetWidth/2 - this.ele.offsetWidth/2 + "px";
+		this.ele.style.top = myPlane.ele.offsetTop - this.ele.offsetHeight + "px";
+		return this;
+	}
+
+//		let init = this.init( ()=>{
+//		
+//		//添加
+//		gameEngine.allBullet[this.id] = this;
+//		console.log( gameEngine.allBullet );
+//		
+//		gameEngine.ele.appendChild(this.ele);
+//		this.ele.className = "bullet";
+//		this.ele.style.left = myPlane.ele.offsetLeft + myPlane.ele.offsetWidth/2 - this.ele.offsetWidth/2 + "px";
+//		this.ele.style.top = myPlane.ele.offsetTop - this.ele.offsetHeight + "px";
+//		return this;
+//	})
+	
+	//方法move
+	move(){
+		let that = this;
+		this.timer = setInterval(function(){
+			let y = that.ele.offsetTop - 10;
+			if (y < -18) {
+				clearInterval(that.timer); //停止移动
+				gameEngine.ele.removeChild(that.ele); //移除子弹
+				
+				//删除
+				delete gameEngine.allBullet[that.id];
+			}
+			else {
+				that.ele.style.top = y + "px"; //移动
+			}
+		}, 50);
+	}
+	
+	//爆炸
+	
+		boom(){
+		clearInterval(this.timer); //停止移动
+		
+		this.ele.className = "bullet-die";
+		
+		let that = this;
+		const dieImgs = ["images2/die1.png", "images2/die2.png"]; 
+		let i = 0;
+		let dieTimer = setInterval(function(){
+			if (i >= 1){
+				clearInterval(dieTimer); //停止爆炸
+				gameEngine.ele.removeChild(that.ele); //移除子弹
+			}else {
+				that.ele.style.background = "url("+ dieImgs[++i] +") no-repeat";
+			}
+		}, 100);
+	}
+}	
+
+
---
+++ code/js/gulpfile.js
@@ -0,0 +1,37 @@
+var obj = {
+	removeComments: true, //清除HTML注释
+	collapseWhitespace: true, //压缩HTML
+	collapseBooleanAttributes: true,//省略布尔属性的值<input checked="true"/> ==> <input checked/>
+	removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+	removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
+	removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+	minifyJS: true, //压缩页面JS
+	minifyCSS: true //压缩页面CSS
+}
+
+//var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+//gulp.task("jsTask",function(){
+//	gulp.src("bullet.js")
+//	.pipe(uglify())
+//	.pipe(gulp.dest("test"))
+//});
+//gulp.task("default",["jsTask"]);
+
+var gulp = require('gulp');  
+var babel = require('gulp-babel');  
+var config={  
+    es6file:'bullet.js',  
+    es5file:'test/'  
+};  
+gulp.task('es6', function () {  
+    return gulp.src(config.es6file)  
+        .pipe(babel({  
+            presets: ['es2015']  
+        }))  
+          .pipe(uglify())  
+        .pipe(gulp.dest(config.es5file));  
+});  
+gulp.task('default', ['es6'], function () {  
+    
+});  
